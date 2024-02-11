14b4d92cba66931217711e432f4402d1689ba0a3
Kin Fei Lee <kilee@seekasia.com>
2024-02-11 21:36:16 +0800

    test: improve test

--- handlers/health_test.go
+++ handlers/health_test.go
@@ -11,15 +11,49 @@ import (
 )
 
 func TestHealth(t *testing.T) {
-	app := fiber.New()
 
-	app.Get("/health", Health)
+	t.Run("return 200 with OK", func(t *testing.T) {
+		app := fiber.New()
+		app.Get("/health", Health)
 
-	resp, err := app.Test(httptest.NewRequest("GET", "/health", nil))
-	assert.Nil(t, err)
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
+		resp, err := app.Test(httptest.NewRequest("GET", "/health", nil))
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
 
-	body, err := io.ReadAll(resp.Body)
-	assert.Nil(t, err)
-	assert.Equal(t, "OK", string(body))
+		body, err := io.ReadAll(resp.Body)
+		assert.Nil(t, err)
+		assert.Equal(t, "OK", string(body))
+	})
+
+	t.Run("return 404 when path does not exist", func(t *testing.T) {
+		app := fiber.New()
+
+		mockTodos := func(c *fiber.Ctx) error {
+			return c.Status(404).SendString("Not Found")
+		}
+
+		app.Get("/health", mockTodos)
+
+		resp, err := app.Test(httptest.NewRequest("GET", "/nonexistent", nil))
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	})
+
+	t.Run("return 500 with Internal Server Error", func(t *testing.T) {
+		app := fiber.New()
+
+		mockHealth := func(c *fiber.Ctx) error {
+			return c.Status(500).SendString("Internal Server error")
+		}
+
+		app.Get("/health", mockHealth)
+
+		resp, err := app.Test(httptest.NewRequest("GET", "/health", nil))
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+
+		body, err := io.ReadAll(resp.Body)
+		assert.Nil(t, err)
+		assert.Equal(t, "Internal Server error", string(body))
+	})
 }
--- handlers/todo_test.go
+++ handlers/todo_test.go
@@ -1,10 +1,10 @@
 package handler
 
 import (
-	"bytes"
 	"encoding/json"
-	"net/http"
+	"io"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
@@ -12,30 +12,96 @@ import (
 )
 
 func TestGetTodos(t *testing.T) {
+	todos = []Todo{
+		{ID: "1", Title: "Todo 1", Status: "pending"},
+		{ID: "2", Title: "Todo 2", Status: "pending"},
+	}
+
 	app := fiber.New()
 	app.Get("/todos", GetTodos)
 
 	resp, err := app.Test(httptest.NewRequest("GET", "/todos", nil))
 	assert.Nil(t, err)
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, fiber.StatusOK, resp.StatusCode)
+
+	// Read the response body
+	body, _ := io.ReadAll(resp.Body)
+
+	// Unmarshal the response body into a slice of Todo structs
+	var returnedTodos []Todo
+	json.Unmarshal(body, &returnedTodos)
+
+	// Assert that the returned todos are equal to the expected todos
+	assert.Equal(t, todos, returnedTodos)
 }
 
 func TestAddTodo(t *testing.T) {
-	app := fiber.New()
-	app.Post("/todos", AddTodo)
+	t.Run("return 400 when body cannot be parsed", func(t *testing.T) {
+		app := fiber.New()
+		app.Post("/todos", AddTodo)
 
-	mockUUID := "test-uuid"
+		body := strings.NewReader("not a valid json")
+		req := httptest.NewRequest("POST", "/todos", body)
+		req.Header.Set("Content-Type", "application/json")
 
-	todo := &Todo{
-		ID:    mockUUID,
-		Title: "Test Todo",
+		resp, err := app.Test(req)
+		assert.Nil(t, err)
+		assert.Equal(t, fiber.StatusBadRequest, resp.StatusCode)
+	})
+
+	t.Run("return 400 when title is empty", func(t *testing.T) {
+		app := fiber.New()
+		app.Post("/todos", AddTodo)
+
+		body := strings.NewReader(`{"title":""}`)
+		req := httptest.NewRequest("POST", "/todos", body)
+		req.Header.Set("Content-Type", "application/json")
+
+		resp, err := app.Test(req)
+		assert.Nil(t, err)
+		assert.Equal(t, fiber.StatusBadRequest, resp.StatusCode)
+	})
+
+	t.Run("return 201 when todo is successfully created", func(t *testing.T) {
+		app := fiber.New()
+		app.Post("/todos", AddTodo)
+
+		body := strings.NewReader(`{"title":"Test Todo"}`)
+		req := httptest.NewRequest("POST", "/todos", body)
+		req.Header.Set("Content-Type", "application/json")
+
+		resp, err := app.Test(req)
+		assert.Nil(t, err)
+		assert.Equal(t, fiber.StatusCreated, resp.StatusCode)
+
+		// Read the response body
+		respBody, _ := io.ReadAll(resp.Body)
+
+		// Unmarshal the response body into a Todo struct
+		var returnedTodo Todo
+		json.Unmarshal(respBody, &returnedTodo)
+
+		// Assert that the returned todo has the expected title
+		assert.Equal(t, "Test Todo", returnedTodo.Title)
+	})
+}
+
+func TestFindTodoByID(t *testing.T) {
+	todos = []Todo{
+		{ID: "1", Title: "Todo 1", Status: "pending"},
+		{ID: "2", Title: "Todo 2", Status: "pending"},
 	}
 
-	todoBytes, _ := json.Marshal(todo)
-	req := httptest.NewRequest("POST", "/todos", bytes.NewBuffer(todoBytes))
-	req.Header.Set("Content-Type", "application/json")
+	t.Run("return nil when no todo found with the given ID", func(t *testing.T) {
+		todo, index := FindTodoByID("3")
+		assert.Nil(t, todo)
+		assert.Equal(t, -1, index)
+	})
 
-	resp, err := app.Test(req)
-	assert.Nil(t, err)
-	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+	t.Run("return todo when todo found with the given ID", func(t *testing.T) {
+		expectedTodo := &todos[0]
+		todo, index := FindTodoByID("1")
+		assert.Equal(t, expectedTodo, todo)
+		assert.Equal(t, 0, index)
+	})
 }
